cdc6de568b92ab5b9798eca19b94a1d67df0ba13
mangonob <534393806@qq.com>
2016-08-28 15:12:23 +0800

    Initial Commit

---
+++ Recorder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recorder
+//
+//  Created by Trinity on 16/8/28.
+//  Copyright © 2016年 Trinity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
